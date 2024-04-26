a527e6aec2c8bf5e200d78d17e244aa730854c63
yeonwoo.kang <kywoo26@naver.com>
2024-04-26 16:52:35 +0900

    first commit

---
+++ src/test/java/PrimeFactorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PrimeFactorTest {
+    @Test
+    void test() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertEquals(10, 10);
+    }
+}
